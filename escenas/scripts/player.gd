96159d732360002fcbebb1e7f6fdfd7d5b09a6e1
Pablo <pablogallinag@gmail.com>
2021-02-04 23:10:04 -0600

    Escena principal creado y movimiento del player y fisicas de la pelota

--- escenas/scripts/player.gd
+++ escenas/scripts/player.gd
@@ -5,6 +5,22 @@ const LEFT : int = -1
 
 export var speed : int = 4
 
+"""
+se ejecuta solo 1 vez
+func _input(event):
+	if event.is_action_pressed("mover_derecha"):
+		position.x += speed
+		
+	if event.is_action_pressed("mover_izquierda"):
+		position.x -= speed
+"""
 func _ready():
 	pass # Replace with function body.
+	
+func _physics_process(delta):
+	if Input.is_action_pressed("mover_derecha"):
+		position.x += speed
+		
+	if Input.is_action_pressed("mover_izquierda"):
+		position.x -= speed
 
